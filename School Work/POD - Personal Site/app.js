abc312af6e8206de4436194a04764718653ea653
SpikerSpiegel <freddy.perez@hyperisland.se>
2020-09-04 19:46:41 +0200

    commit on master

--- School Work/POD - Personal Site/app.js
+++ School Work/POD - Personal Site/app.js
@@ -3,7 +3,6 @@ const navSlide = () => {
   const nav = document.querySelector(".nav-links");
   const navLinks = document.querySelectorAll(".nav-links li");
 
-
   //Toggle Nav
   burger.addEventListener("click", () => {
     nav.classList.toggle("nav-active");
