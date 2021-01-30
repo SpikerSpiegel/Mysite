cb4284d9c5888cd0eca65027841f50ebdb9c3cb2
Alestry Pérez <alestry.perez@hyperisland.se>
2021-01-30 10:28:23 +0100

    commit

--- app.js
+++ app.js
@@ -1,51 +1,40 @@
-const burger = document.querySelector(".burger");
-const line1 = document.querySelector(".line1");
-const line3 = document.querySelector(".line3");
-
-
+//const burger = document.querySelector(".burger");
+//const line1 = document.querySelector(".line1");
+//const line3 = document.querySelector(".line3");
 const navSlide = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-links");
   const navLinks = document.querySelectorAll(".nav-links li");
-
   //Toggle Nav
   burger.addEventListener("click", () => {
     nav.classList.toggle("nav-active");
-
     //Animate Links
     navLinks.forEach((link, index) => {
       if (link.style.animation) {
         link.style.animation = "";
-
       } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.3
+        }s`;
       }
     });
     //Burger Animation
-    burger.classList.toggle('toggle');
-
+    burger.classList.toggle("toggle");
   });
-
 };
-
-navSlide()
-
-burger.addEventListener("mouseover", hover)
-burger.addEventListener("mouseleave", hoverOff)
-
+navSlide();
+burger.addEventListener("mouseover", hover);
+burger.addEventListener("mouseleave", hoverOff);
 function hover(event) {
-  if (!event.target.classList.contains('toggle')) {
-    line1.style.marginBottom = '0.4rem'
-    line3.style.marginTop = '0.4rem'
+  if (!event.target.classList.contains("toggle")) {
+    line1.style.marginBottom = "0.4rem";
+    line3.style.marginTop = "0.4rem";
   }
   // event.target.style.background = 'red'
-
 }
-
 function hoverOff() {
-  if (!event.target.classList.contains('toggle')) {
-    line1.style.marginBottom = '0.3rem'
-    line3.style.marginTop = '0rem'
+  if (!event.target.classList.contains("toggle")) {
+    line1.style.marginBottom = "0.3rem";
+    line3.style.marginTop = "0rem";
   }
 }
